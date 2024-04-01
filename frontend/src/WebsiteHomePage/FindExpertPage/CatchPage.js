5f467eed2ad0ac0be9238445681aaee2cc0a3c5c
sandyyy646 <honeychakkal101@gmail.com>
2024-04-01 14:09:31 -0700

    Added email validation + update waitlist page

--- frontend/src/WebsiteHomePage/FindExpertPage/CatchPage.js
+++ frontend/src/WebsiteHomePage/FindExpertPage/CatchPage.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import axios from 'axios';
 import './CatchPage.css'; // Import CSS file for styling
 import { responsiveFontSizes } from '@mui/material';
+import validators from './../../validators';
 
 const CatchPage = () => {
     const [email, setEmail] = useState('');
     const [userType, setUserType] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [showErrorModal, setErrorModal] = useState(false);
+    const [emailError, setEmailError] = useState('');
 
     const handleEmailChange = (event) => {
-        setEmail(event.target.value);
+        const { value } = event.target;
+        setEmail(value);
+        // Check email validity
+        if (!validators.isValidEmail(value)) {
+            setEmailError('Please enter a valid email address.');
+        } else {
+            setEmailError('');
+        }
     };
 
     const handleJoinButtonClick = async (type) => {
         setUserType(type);
+        // Check if email is valid
+        if (!validators.isValidEmail(email)) {
+            setEmailError('Please enter a valid email address.');
+            return;
+        }
         try {
             const formData = new FormData();
             formData.append('email', email);
@@ -29,8 +43,10 @@ const CatchPage = () => {
             const response = await axios.post(endpoint, formData);
 
             if (response.status === 200) {
+                console.log('logging out the success');
                 toggleModal(); 
             } else {
+                console.log('logging out the error');
                 toggleErrorModal();
             }
 
@@ -39,7 +55,7 @@ const CatchPage = () => {
             toggleErrorModal();
             console.error('Error submitting data:', error);
         }
-    };
+    }
 
     const toggleModal = () => {
         setShowModal(!showModal);
@@ -61,21 +77,34 @@ const CatchPage = () => {
         );
     };
 
-    const Waitlist = () => {
+    const ErrorModal = ({ onClose }) => {
+        return (
+            <div className="error-modal-container">
+                <div className="error-modal">
+                    <div className="error-modal-content">
+                        <h2>Error</h2>
+                        <p>There was an error while submitting your response. Please try again.</p>
+                        <button onClick={onClose}>Close</button>
+                    </div>
+                </div>
+            </div>
+        );
+    };
 
+    const Waitlist = () => {
         return (
             <div className="catch-page">
                 <h1>Coming soon!</h1>
                 <h2>Discover a platform where experts and advice-seekers unite for valuable exchanges and genuine connections.</h2>
                 <h4>Say goodbye to unanswered messages and hello to opportunity. Join our community today to monetize your expertise, gain valuable insights, and propel your journey forward. Sign up now to be part of the future connections.</h4>
                 <div className="email-field">
-                    <input type="email" placeholder="Enter your email" value={email} onChange={handleEmailChange} />
+                    <input type="email" placeholder="Enter your email" value={email} onChange={handleEmailChange} autoFocus/>
+                    {emailError && <span className="error-message">{emailError}</span>}
                     <div className="join-button">
                         <button className="join-button1" onClick={() => handleJoinButtonClick('Client')}>Join as Advice Seeker</button>
                         <button className="join-button2" onClick={() => handleJoinButtonClick('Expert')}>Join as Expert</button>
                     </div>
                 </div>
-                {showModal && <Modal />}
             </div>
         );
     };
@@ -83,38 +112,11 @@ const CatchPage = () => {
     return (
         <div>
             {!showModal && <Waitlist />}
-            {showModal && <Modal />}
+            {showModal && <Modal onClose={toggleModal} />}
+            {showErrorModal && <ErrorModal onClose={toggleErrorModal} />}
         </div>
     );
 };
 export default CatchPage;
 
-
-
-//     {/* const Waitlist = () => {
-
-//         return (
-//             <div className="catch-page">
-//                 <h1>Coming soon!</h1>
-//                 <h2>Discover a platform where experts and advice-seekers unite for valuable exchanges and genuine connections.</h2>
-//                 <h4>Say goodbye to unanswered messages and hello to opportunity. Join our community today to monetize your expertise, gain valuable insights, and propel your journey forward. Sign up now to be part of the future connections.</h4>
-//                 <div className="email-field">
-//                     <input type="email" placeholder="Enter your email" value={email} onChange={handleEmailChange} />
-//                     <div className="join-button">
-//                         <button className="join-button1" onClick={() => handleJoinButtonClick('Advice Seeker')}>Join as Advice Seeker</button>
-//                         <button className="join-button2" onClick={() => handleJoinButtonClick('Expert')}>Join as Expert</button>
-//                     </div>
-
-//                 </div>
-//             </div>
-//         );
-//     }; */}
-
-//     return (
-//         <div>
-//             {!showModal && <Waitlist />}
-            
-//         </div>
-//     );
-// };
 
